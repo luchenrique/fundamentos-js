64746388cd1155388800d4192d613759098dd245
Seu Nome <lucas.barbosa3680@gmail.com>
2023-02-12 21:46:02 -0300

    exemplos básicos

---
+++ type-boolean.js
@@ -0,0 +1,7 @@
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
---
+++ type-string.js
@@ -0,0 +1,17 @@
+// const texto = 'Meu nome é'
+// const nome = 'Lucas'
+// console.log(texto, nome)
+
+
+// const cidade = "belo horizonte";
+// const input = "Belo Horizonte";
+
+// console.log(cidade === input); // false
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo);
+console.log(inputMinusculo.length)
